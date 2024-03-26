bc98e4734be23cfe3b9cf2956194ed93358c579e
passivikas09 <140304281+passivikas09@users.noreply.github.com>
2024-03-26 18:52:32 +0530

    env

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express =require("express")
 require("dotenv").config()
 const app= express()
 const port=process.env.PORT|| 4000
-
 const userRouter=require('./Routes/userRoute')
 const adminRouter=require("./Routes/adminRoute")
 const admin=require("./db/seed")
