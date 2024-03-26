f28037cf4d6b925ca5db095f270716668a81ffc5
passivikas09 <140304281+passivikas09@users.noreply.github.com>
2024-03-26 18:36:42 +0530

    cors related

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require("express")
 require("dotenv").config()
 const app= express()
-const port=process.env.PORT|| "http://localhost:5000/"
+const port=process.env.PORT|| 4000
 
 const userRouter=require('./Routes/userRoute')
 const adminRouter=require("./Routes/adminRoute")
@@ -9,7 +9,11 @@ const admin=require("./db/seed")
 const cors=require('cors')
 require("./db/config")
 app.use(express.static(__dirname+"/public/"))
-app.use(cors())
+app.use(cors({
+    origin:"https://urban-front.vercel.app",
+    methods:['GET', 'POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.urlencoded())
 app.use(express.json())
 
